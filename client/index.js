7399dbbc3c79d13743cbc50e43441293bc3d3a98
ykenichi <yama.kenichi@yahoo.com>
2017-06-10 13:40:28 -0400

    Added example router and angular html

--- client/index.js
+++ client/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 
 app.get('/', function (req, res) {
   res.send('Hello World2!')
+})
+
+app.get('/test', function (req, res) {
+  res.sendFile(path.join(__dirname+'/html/index.html'));
 })
 
 app.listen(3000, function () {
