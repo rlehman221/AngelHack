4066caee262962d5c6c2b593e6620176c91b2378
ykenichi <yama.kenichi@yahoo.com>
2017-06-10 21:46:14 -0400

    Added client server routes

--- client/index.js
+++ client/index.js
@@ -1,15 +1,55 @@
-const express = require('express')
-const app = express()
-var path = require('path');
+const express = require('express')  
+const app = express()  
+const port = 5000
+const loopback_url = 'http://0.0.0.0:3000/api'
+var request = require('request')
 
-app.get('/', function (req, res) {
-  res.send('Hello World2!')
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {  
+
+  request(loopback_url + '/students', function (error, response, body) {
+    if(error)
+      throw error;
+    console.log('error:', error); // Print the error if one occurred 
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+    console.log('body:', body); // Print the HTML for the Google homepage. 
+    //res.render('index',body);
+  });
 })
 
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname+'/html/index.html'));
+app.get('/usertype', (req, res) => {  
+  res.send('user type button');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/ownersignup', (req, res) => {  
+  res.send('owner signs up');
+})
+
+app.get('/studentsignup', (req, res) => {  
+  res.send('student signs up');
+})
+
+app.get('/jobprofile', (req, res) => {  
+  res.send('job profile');
+})
+
+app.get('/studentprofile', (req, res) => {  
+  res.send('student profile');
+})
+
+app.get('/questionnaire', (req, res) => {  
+  res.send('questionnaire');
+})
+
+app.get('/addjob', (req, res) => {  
+  res.send('add job');
+})
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('An error has occurred', err)
+  }
+
+  console.log(`server is listening on ${port}`)
 })
