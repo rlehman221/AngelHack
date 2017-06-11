bdf3519d8f7123913e01bc742d8824f5e9815c09
dave8284 <jsdavesiddharth@gmail.com>
2017-06-11 11:15:07 -0400

    login page

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+
+
+const express = require('express');
+const app = express();
 var path = require('path');
 
 app.get('/', function (req, res) {
@@ -13,3 +15,38 @@ app.get('/test', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+
+angular.module('buttonsDemo1', ['ngMaterial'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+
+  $scope.googleUrl = 'http://google.com';
+
+});
+
+
+
+function changeToBusiness(){
+
+	document.getElementById('student').style.display = "none";
+	document.getElementById('student').style.visibility = "hidden";
+
+	document.getElementById('business').style.display = "block";
+	document.getElementById('business').style.visibility = "visible";
+}
+
+function changeToStudent(){
+
+
+	document.getElementById('business').style.display = "none";
+	document.getElementById('business').style.visibility = "hidden";
+
+	document.getElementById('student').style.display = "block";
+	document.getElementById('student').style.visibility = "visible";
+}
+
